4db49d5d1e17ce937d5e3b62c07c2d66ad01cd61
Doğan EFE <dodoman20@windowslive.com>
2019-07-03 16:14:18 +0300

    duzeltmeler

--- HaberSite/Controllers/ContactController.cs
+++ HaberSite/Controllers/ContactController.cs
@@ -18,20 +18,7 @@ namespace HaberSite.Controllers
         // GET: Contact
         public ActionResult Index()
         {
-            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
-            string url = "https://www.sozcu.com.tr/sitemap_google_news.xml";
-
-            XmlSerializer ser = new XmlSerializer(typeof(Urlset));
-
-            WebClient client = new WebClient();
-
-            string data = Encoding.Default.GetString(client.DownloadData(url));
-
-            Stream stream = new MemoryStream(Encoding.UTF8.GetBytes(data));
-
-            Urlset reply = (Urlset)ser.Deserialize(stream);
-
-            return Content(reply.urlList[2].image[0].imageloc);
+            return View();
         }
     }
 }
