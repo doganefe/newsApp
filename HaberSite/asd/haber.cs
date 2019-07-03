181b2d4bd220666c67e8eb09d564a074b50900b2
Doğan EFE <dodoman20@windowslive.com>
2019-07-03 16:06:57 +0300

    ufak isler

---
+++ HaberSite/asd/haber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HaberSite.Models
+{
+    public class haber
+    {
+        public string status { get; set; }
+        public List<Item> articles { get; set; }
+        public int totalResults { get; set; }
+
+    }
+    public class Item
+    {
+        public string title { get; set; }
+        public string content { get; set; }
+    }
+}
